4b102264941385dda3564c1412a7913f90a2483a
Jeremy Shantz <jeremy@shantz.io>
2014-08-30 23:28:51 -0400

    initial commit

---
+++ js/reactApp.js
@@ -0,0 +1,68 @@
+/** @jsx React.DOM */
+'use strict';
+
+var outer = React.createClass({
+
+    getInitialState: function () {
+
+        return this.props.model; // probably an anti-pattern; use name property to pull state from factory object
+    },
+
+    toggleDisplay: function () {
+        this.setState({ visible: !this.state.visible });
+    },
+
+    render: function () {
+
+        var model = this.state;
+
+        var classes = React.addons.classSet({
+            'wrapper': true,
+            'hide': !model.visible
+        });
+
+        var child = React.addons.cloneWithProps(React.Children.only(this.props.children), {
+            model: this.state,
+            toggleDisplay: this.toggleDisplay
+        });
+
+        return (
+            <div className={classes}><h4>Wrapper of {model.name}</h4>{child}</div>
+            );
+    }
+});
+
+var inner = React.createClass({
+
+    toggleDisplay: function (event) {
+        this.props.toggleDisplay();
+    },
+    render: function () {
+
+        var model = this.props.model;
+
+        return (
+            <div className="inner"><h5>Implementation of {model.message}</h5>
+                <input type="checkbox" checked={model.visible} onChange={this.toggleDisplay} /> Show
+            </div>
+            );
+    }
+});
+
+var app = React.createClass({
+
+    render: function() {
+
+        var models = this.props.data.map(function(item, i){
+
+            return <outer key={i} model={item} ><inner /></outer>
+        });
+
+        return (<div><h2>React</h2>{models}</div>);
+    }
+});
+
+React.renderComponent(
+    <app data={window.FormData} />, document.getElementById('react')
+    // later we can wrap the reference to window.FormData in a factory object, and eventually have the object fetch the data from the server
+);
---
+++ js/vendors/ecmascript6.polyfills.js
@@ -0,0 +1,61 @@
+﻿// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+
+if (!Array.prototype.find) {
+    Object.defineProperty(Array.prototype, 'find', {
+        enumerable: false,
+        configurable: true,
+        writable: true,
+        value: function (predicate) {
+            if (this == null) {
+                throw new TypeError('Array.prototype.find called on null or undefined');
+            }
+            if (typeof predicate !== 'function') {
+                throw new TypeError('predicate must be a function');
+            }
+            var list = Object(this);
+            var length = list.length >>> 0;
+            var thisArg = arguments[1];
+            var value;
+
+            for (var i = 0; i < length; i++) {
+                if (i in list) {
+                    value = list[i];
+                    if (predicate.call(thisArg, value, i, list)) {
+                        return value;
+                    }
+                }
+            }
+            return undefined;
+        }
+    });
+}
+
+if (!Array.prototype.findIndex) {
+    Object.defineProperty(Array.prototype, 'findIndex', {
+        enumerable: false,
+        configurable: true,
+        writable: true,
+        value: function (predicate) {
+            if (this == null) {
+                throw new TypeError('Array.prototype.find called on null or undefined');
+            }
+            if (typeof predicate !== 'function') {
+                throw new TypeError('predicate must be a function');
+            }
+            var list = Object(this);
+            var length = list.length >>> 0;
+            var thisArg = arguments[1];
+            var value;
+
+            for (var i = 0; i < length; i++) {
+                if (i in list) {
+                    value = list[i];
+                    if (predicate.call(thisArg, value, i, list)) {
+                        return i;
+                    }
+                }
+            }
+            return -1;
+        }
+    });
+}
